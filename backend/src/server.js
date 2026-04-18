f7b31caf194cd30bd9d4b098e857b43f153f2fb1
Avnishkr01 <avnish011kumar.com>
2026-04-18 17:43:01 +0530

    changes url

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,11 +13,11 @@ const PORT = process.env.PORT;
 
 const __dirname = path.resolve();
 
-// app.use(cors({
-//     origin: "https://streamify-frontend-0fom.onrender.com",
-//     credentials: true,
-// }));
-app.use(cors());
+app.use(cors({
+    origin: "https://streamify-platform-frontend.onrender.com",
+    credentials: true,
+}));
+// app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
