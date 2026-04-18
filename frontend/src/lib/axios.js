a54eb2d3a525d77c04e866399585e9ceb0ac66f3
Avnishkr01 <avnish011kumar.com>
2026-04-18 17:31:14 +0530

    base url changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? `${import.meta.env.VITE_BACKEND_URL}/api` : "/api"
+// const BASE_URL = import.meta.env.MODE === "development" ? `${import.meta.env.VITE_BACKEND_URL}/api` : "/api"
+const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/api`;
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
